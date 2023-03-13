88c010b867124cfe7d049750cae40a1f36f6bd08
Gabriele Evangelista <gabryx28@hotmail.it>
2023-03-13 18:34:46 +0100

    read input file only once

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,21 +9,47 @@ import scala.language._
 object main {
   def main(args: Array[String]): Unit = {
     // 0: sequential, 1: parallel, 2: distributed
-    val execution = 1
+    val execution = 0
     // verbosity of the output
     val verbose = true
     // name of the file containing the considered dataset
     val fileName: String = "train_triplets_50k.txt"
     // number of (maximum) users and songs to consider
-    val nUsedUsers = 500
-    val nUsedSongs = 500
+    val nUsedUsers = 100
+    val nUsedSongs = 100
 
     // import dataset
     def in: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource(fileName).getPath)
-    // load all users
-    val users = in.getLines().toList map (line => line split "\t" slice(0, 1) mkString) distinct
-    // load all songs
-    val songs = in.getLines().toList map (line => line split "\t" slice(1, 2) mkString) distinct
+
+    def songsByUser(): (List[String], List[String], Map[String, List[String]]) = {
+      // map user1->[{songs listened by user1}], ..., userN->[{songs listened by userN}]
+      val songsUsersMap = collection.mutable.Map[String, List[String]]()
+      // all users in file
+      val usersInFile = collection.mutable.Set[String]()
+      // all songs in file
+      val songsInFile = collection.mutable.Set[String]()
+      // for each split line on "\t"
+      for {
+        line <- in.getLines().toList
+      } yield line split "\t" match {
+        case Array(u, s, _) => {
+          // add user and song
+          usersInFile add u
+          songsInFile add s
+          // update map with cases
+          songsUsersMap.updateWith(u) {
+            // if user is already in the map, add song to the list of listened songs
+            case Some(list: List[String]) => Some(list :+ s)
+            // else add song to a new list related to the user
+            case None => Some(List(s))
+          }
+        }
+      }
+      (usersInFile.toList , songsInFile.toList , songsUsersMap.toMap)
+    }
+
+    // get data from file
+    val (users, songs, usersToSongsMap) = songsByUser()
 
     // print number of total users and songs in the file
     if (verbose) println(s"File \'$fileName\' contains ${users.length} users and ${songs.length} songs")
@@ -44,10 +70,6 @@ object main {
     } yield line split "\t" match {
       case Array(_, song, _) => song
     }) distinct
-
-    // create a map user1->[{songs listened by user1}], ..., userN->[{songs listened by userN}]
-    // making this structure an IterableOnce with parallelization in mind does not improve performances
-    val usersToSongsMap = usedUsers.iterator.toSeq.map(user => user -> songsFilteredByUser(user)) toMap
 
     // instantiate musicRecommender
     val musicRecommender: MusicRecommender = new MusicRecommender(usedUsers, usedSongs, usersToSongsMap, execution)
