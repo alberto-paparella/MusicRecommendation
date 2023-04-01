8e3d8ffe6ac4871294ee3aa244e20ff506520b3a
alberto-paparella <js.flakes46@gmail.com>
2023-04-01 13:40:51 +0200

    Refactorised ubm with train and test

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -12,16 +12,22 @@ object main {
     // verbosity of the output
     val verbose = true
     // name of the file containing the considered dataset
-    val fileName: String = "train_triplets_5k.txt"
+    //val fileName: String = "train_triplets_5k.txt"
 
     // import dataset
-    def in: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource(fileName).getPath)
+    //def in: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource(fileName).getPath)
+
+    // import train and test datasets
+    def train: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource("train_100_10.txt").getPath)
+    def test: BufferedSource = Source.fromFile(getClass.getClassLoader.getResource("test_100_10.txt").getPath)
+    if (verbose) println("Loaded files")
 
     /*
      * We could have used a single map, but having both can be useful for some tricks later on
      * Between space and temporal efficiency, we chose the latter
      */
-    def songsByUser(): (List[String], List[String], Map[String, List[String]], Map[String, List[String]]) = {
+    /*
+    def songsByUser(in: BufferedSource): (List[String], List[String], Map[String, List[String]], Map[String, List[String]]) = {
       // all users in file
       val usersInFile = collection.mutable.Set[String]()
       // all songs in file
@@ -55,46 +61,25 @@ object main {
       }
       (usersInFile.toList , songsInFile.toList , songsUsersMap.toMap, usersSongsMap.toMap)
     }
+    */
 
     // get data from file
-    val (users, songs, usersToSongsMap, songsToUsersMap) = songsByUser()
+    //val (users, songs, usersToSongsMap, songsToUsersMap) = songsByUser()
+
+    // get train and test data from file
+    //val (trainUsers, trainSongs, trainUsersToSongsMap, trainSongsToUsersMap) = songsByUser(train)
+    //val (testUsers, testSongs, testUsersToSongsMap, testSongsToUsersMap) = songsByUser(test)
 
     // print number of total users and songs in the file
-    if (verbose) println(s"File \'$fileName\' contains ${users.length} users and ${songs.length} songs")
-
-    val trainUsers = users.grouped(users.length-10).toList.head
-    val testUsers = users.grouped(users.length-10).toList(1)
-
-    val trainSet = trainUsers.map(u => (u,usersToSongsMap(u))).toMap
-    val testLabels = testUsers.map(u => (u, usersToSongsMap(u))).toMap
-
-    def calTestSet(testLabels: Map[String, List[String]]) = {
-      for {u <- testLabels}
-        yield (u._1, u._2.grouped((u._2.length / 2) + 1).toList.head)
-    }
-
-    val testSet = calTestSet(testLabels)
-
-    /*
-    for {u <- testLabels}
-      println(u._2.length)
-
-    println("##########################")
-
-    for {u <- testSet}
-      println(u._2.length)
-     */
-
-    print(testLabels.head)
-
-    //val input = trainSet ++ testSet
+    //if (verbose) println(s"File \'$fileName\' contains ${users.length} users and ${songs.length} songs")
 
     // instantiate musicRecommender
     //val musicRecommender: MusicRecommender = new MusicRecommender(users, songs, usersToSongsMap, songsToUsersMap, execution)
-    //val musicRecommender: MusicRecommender = new MusicRecommender(users, songs, input, songsToUsersMap, execution)
+    val musicRecommender: MusicRecommender = new MusicRecommender(train, test, execution)
+    if (verbose) println("MusicRecommender instanced")
 
     // calculating user-based model
-    //musicRecommender.getUserBasedModel()
+    musicRecommender.getUserBasedModel()
     //musicRecommender.getItemBasedModel()
     //musicRecommender.getLinearCombinationModel(0.5)
     //musicRecommender.getAggregationModel(0.5)
